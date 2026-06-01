7fd9a54913d00fe6cf1c9ab5c24c02aca781bf26
wpsadi <wpsadi@outlook.com>
2026-06-01 23:07:35 +0530

    update

--- src/utils/proxyFetch.ts
+++ src/utils/proxyFetch.ts
@@ -245,25 +245,35 @@ function getUrlFromInput( input: FetchInput ): URL | undefined {
 
 type NodeRequestListener = ( req: http.IncomingMessage, res: http.ServerResponse ) => void;
 
-export function createNodeServerWithNoDelay( requestListener?: NodeRequestListener ): http.Server {
-    return http.createServer( ( req, res ) => {
+function wrapNoDelayListener( requestListener?: NodeRequestListener ): NodeRequestListener | undefined {
+    if ( !requestListener ) {
+        return undefined;
+    }
+
+    return ( req, res ) => {
         const socket = res.socket;
         if ( socket ) {
             socket.setNoDelay( true );
             socket.setKeepAlive( true, KEEP_ALIVE_ENABLE_MS );
         }
-        if ( requestListener ) {
-            requestListener( req, res );
-        }
-    } );
+        requestListener( req, res );
+    };
+}
+
+export function createNodeServerWithNoDelay( requestListener?: NodeRequestListener ): http.Server {
+    return http.createServer( wrapNoDelayListener( requestListener ) );
 }
 
 export function createNodeServerFactoryWithNoDelay(): typeof http.createServer {
     const factory = ( ( ...args: any[] ) => {
-        if ( typeof args[0] === 'function' ) {
-            return createNodeServerWithNoDelay( args[0] as NodeRequestListener );
+        if ( typeof args[0] === 'function' || args.length === 0 ) {
+            return http.createServer( wrapNoDelayListener( args[0] as NodeRequestListener | undefined ) );
         }
-        return createNodeServerWithNoDelay();
+
+        const requestListener = typeof args[1] === 'function'
+            ? wrapNoDelayListener( args[1] as NodeRequestListener )
+            : undefined;
+        return http.createServer( args[0], requestListener );
     } ) as unknown as typeof http.createServer;
     return factory;
 }
